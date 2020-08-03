009e9ced249435e8cdc0ab9bb30e0176202c2947
Fabianofski <friedrich.fabian@gmx.net>
2020-08-03 17:30:56 +0200

    Files restored

--- Brackeys Game Jam/Assets/Scripts/Input System/InputMaster1.cs
+++ Brackeys Game Jam/Assets/Scripts/Input System/InputMaster1.cs
@@ -1,4 +1,4 @@
-// GENERATED AUTOMATICALLY FROM 'Assets/Scripts/Input System/InputMaster.inputactions'
+// GENERATED AUTOMATICALLY FROM 'Assets/Scripts/Input System/InputMaster1.inputactions'
 
 using System;
 using System.Collections;
@@ -6,30 +6,54 @@ using System.Collections.Generic;
 using UnityEngine.InputSystem;
 using UnityEngine.InputSystem.Utilities;
 
-public class @InputMaster : IInputActionCollection, IDisposable
+public class @InputMaster1 : IInputActionCollection, IDisposable
 {
     public InputActionAsset asset { get; }
-    public @InputMaster()
+    public @InputMaster1()
     {
         asset = InputActionAsset.FromJson(@"{
-    ""name"": ""InputMaster"",
+    ""name"": ""InputMaster1"",
     ""maps"": [
         {
             ""name"": ""Player"",
-            ""id"": ""99c8ec3a-c577-4247-a068-fffb03b7bb07"",
+            ""id"": ""4cd54e6f-a0e0-4502-a1df-a5ecd46ddd6b"",
             ""actions"": [
                 {
                     ""name"": ""Movement"",
                     ""type"": ""Value"",
-                    ""id"": ""69f532c8-77a7-4f4e-8b42-512d1101c1a7"",
+                    ""id"": ""00f3f5b7-5723-4084-897d-18788bdd9945"",
                     ""expectedControlType"": ""Vector2"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Sprint"",
+                    ""type"": ""Button"",
+                    ""id"": ""1075b305-ba1e-41f7-a355-2ec10cfe7938"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Crouch"",
+                    ""type"": ""Button"",
+                    ""id"": ""2e55ae66-8eda-4b80-beae-8dce92a32531"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Punch"",
+                    ""type"": ""Button"",
+                    ""id"": ""624be8d8-5c56-4835-9cdb-7a1b7b17981b"",
+                    ""expectedControlType"": ""Button"",
                     ""processors"": """",
                     ""interactions"": """"
                 },
                 {
                     ""name"": ""Rewind"",
                     ""type"": ""Button"",
-                    ""id"": ""2e991815-01b6-48b8-8949-b8b33fde636c"",
+                    ""id"": ""0aa89c3d-42ef-4f01-ade2-2bf203982042"",
                     ""expectedControlType"": ""Button"",
                     ""processors"": """",
                     ""interactions"": """"
@@ -37,8 +61,8 @@ public class @InputMaster : IInputActionCollection, IDisposable
             ],
             ""bindings"": [
                 {
-                    ""name"": ""Horizontal"",
-                    ""id"": ""0c0b6ecb-9ebf-4bf6-9a50-295917b5f0a8"",
+                    ""name"": ""movement"",
+                    ""id"": ""f6446718-4d4e-463b-8dbd-6707ffd1a926"",
                     ""path"": ""2DVector"",
                     ""interactions"": """",
                     ""processors"": """",
@@ -48,8 +72,8 @@ public class @InputMaster : IInputActionCollection, IDisposable
                     ""isPartOfComposite"": false
                 },
                 {
-                    ""name"": ""Up"",
-                    ""id"": ""63bcf13b-4877-4baa-932b-f22a28e91d77"",
+                    ""name"": ""up"",
+                    ""id"": ""a661c21d-8acb-4815-b2b1-4d4080b2fa4f"",
                     ""path"": ""<Keyboard>/w"",
                     ""interactions"": """",
                     ""processors"": """",
@@ -59,8 +83,8 @@ public class @InputMaster : IInputActionCollection, IDisposable
                     ""isPartOfComposite"": true
                 },
                 {
-                    ""name"": ""Down"",
-                    ""id"": ""8de543ef-d375-475b-ab20-8e0305bf56e3"",
+                    ""name"": ""down"",
+                    ""id"": ""e6ae11ff-78f7-4892-a639-945bc53b2b23"",
                     ""path"": ""<Keyboard>/s"",
                     ""interactions"": """",
                     ""processors"": """",
@@ -70,8 +94,8 @@ public class @InputMaster : IInputActionCollection, IDisposable
                     ""isPartOfComposite"": true
                 },
                 {
-                    ""name"": ""Left"",
-                    ""id"": ""19353261-d79d-488b-8892-94d1dbb2e457"",
+                    ""name"": ""left"",
+                    ""id"": ""c2ea7de7-b28d-4d93-9193-a2bf83766112"",
                     ""path"": ""<Keyboard>/a"",
                     ""interactions"": """",
                     ""processors"": """",
@@ -81,8 +105,8 @@ public class @InputMaster : IInputActionCollection, IDisposable
                     ""isPartOfComposite"": true
                 },
                 {
-                    ""name"": ""Right"",
-                    ""id"": ""ca4387ea-5462-467c-bd8f-f22b76ba8cbf"",
+                    ""name"": ""right"",
+                    ""id"": ""e0994a3d-3038-461a-9e70-3d9fabcd09b1"",
                     ""path"": ""<Keyboard>/d"",
                     ""interactions"": """",
                     ""processors"": """",
@@ -92,133 +116,45 @@ public class @InputMaster : IInputActionCollection, IDisposable
                     ""isPartOfComposite"": true
                 },
                 {
-                    ""name"": ""Joystick Horizontal"",
-                    ""id"": ""a24d055f-d448-455a-94ba-dbe219701b9c"",
-                    ""path"": ""2DVector"",
+                    ""name"": """",
+                    ""id"": ""4a113480-5562-492d-ba5e-0019e9c2b9d6"",
+                    ""path"": ""<Keyboard>/shift"",
                     ""interactions"": """",
                     ""processors"": """",
-                    ""groups"": """",
-                    ""action"": ""Movement"",
-                    ""isComposite"": true,
+                    ""groups"": ""Keyboard and Mouse"",
+                    ""action"": ""Sprint"",
+                    ""isComposite"": false,
                     ""isPartOfComposite"": false
-                },
-                {
-                    ""name"": ""Up"",
-                    ""id"": ""fc62577f-9668-402a-a9e7-591f64e5da89"",
-                    ""path"": ""<Gamepad>/leftStick/left"",
-                    ""interactions"": """",
-                    ""processors"": """",
-                    ""groups"": ""Gamepad"",
-                    ""action"": ""Movement"",
-                    ""isComposite"": false,
-                    ""isPartOfComposite"": true
-                },
-                {
-                    ""name"": ""Down"",
-                    ""id"": ""db074fea-3313-4328-aabb-f157b0e0aa17"",
-                    ""path"": ""<Gamepad>/leftStick/right"",
-                    ""interactions"": """",
-                    ""processors"": """",
-                    ""groups"": ""Gamepad"",
-                    ""action"": ""Movement"",
-                    ""isComposite"": false,
-                    ""isPartOfComposite"": true
-                },
-                {
-                    ""name"": ""Left"",
-                    ""id"": ""08d6a201-9a76-42e6-b6c4-a864387ca8e1"",
-                    ""path"": """",
-                    ""interactions"": """",
-                    ""processors"": """",
-                    ""groups"": """",
-                    ""action"": ""Movement"",
-                    ""isComposite"": false,
-                    ""isPartOfComposite"": true
-                },
-                {
-                    ""name"": ""Right"",
-                    ""id"": ""b6f2cee4-c1a1-4908-97a7-cecbb28c6312"",
-                    ""path"": """",
-                    ""interactions"": """",
-                    ""processors"": """",
-                    ""groups"": """",
-                    ""action"": ""Movement"",
-                    ""isComposite"": false,
-                    ""isPartOfComposite"": true
-                },
-                {
-                    ""name"": ""D-Pad Horizontal"",
-                    ""id"": ""7e2b009e-bb11-46f8-9378-7cbefea6242f"",
-                    ""path"": ""2DVector"",
-                    ""interactions"": """",
-                    ""processors"": """",
-                    ""groups"": """",
-                    ""action"": ""Movement"",
-                    ""isComposite"": true,
-                    ""isPartOfComposite"": false
-                },
-                {
-                    ""name"": ""Up"",
-                    ""id"": ""9dea1a79-4cfe-492b-b221-e5c317383e56"",
-                    ""path"": ""<Gamepad>/dpad/left"",
-                    ""interactions"": """",
-                    ""processors"": """",
-                    ""groups"": ""Gamepad"",
-                    ""action"": ""Movement"",
-                    ""isComposite"": false,
-                    ""isPartOfComposite"": true
-                },
-                {
-                    ""name"": ""Down"",
-                    ""id"": ""e3eb058f-2da1-49a0-b7d6-196f0adfbf9d"",
-                    ""path"": ""<Gamepad>/dpad/right"",
-                    ""interactions"": """",
-                    ""processors"": """",
-                    ""groups"": ""Gamepad"",
-                    ""action"": ""Movement"",
-                    ""isComposite"": false,
-                    ""isPartOfComposite"": true
-                },
-                {
-                    ""name"": ""Left"",
-                    ""id"": ""9a931742-a156-4dbb-9a7b-1ca804ee1d55"",
-                    ""path"": """",
-                    ""interactions"": """",
-                    ""processors"": """",
-                    ""groups"": """",
-                    ""action"": ""Movement"",
-                    ""isComposite"": false,
-                    ""isPartOfComposite"": true
-                },
-                {
-                    ""name"": ""Right"",
-                    ""id"": ""d0853c56-2a65-4524-90aa-e078505822a1"",
-                    ""path"": """",
-                    ""interactions"": """",
-                    ""processors"": """",
-                    ""groups"": """",
-                    ""action"": ""Movement"",
-                    ""isComposite"": false,
-                    ""isPartOfComposite"": true
                 },
                 {
                     ""name"": """",
-                    ""id"": ""aa5f24ef-3139-4fa0-a314-567fa229aa59"",
+                    ""id"": ""3f881ee9-2533-4faf-b053-499113effab9"",
+                    ""path"": ""<Keyboard>/leftCtrl"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard and Mouse"",
+                    ""action"": ""Crouch"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""279f2c1f-2b6e-492f-a18c-07d54849ae4e"",
+                    ""path"": ""<Keyboard>/space"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard and Mouse"",
+                    ""action"": ""Punch"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""b28f74da-8d6a-4fb6-97b4-1b996540d54e"",
                     ""path"": ""<Keyboard>/q"",
                     ""interactions"": """",
                     ""processors"": """",
                     ""groups"": ""Keyboard and Mouse"",
-                    ""action"": ""Rewind"",
-                    ""isComposite"": false,
-                    ""isPartOfComposite"": false
-                },
-                {
-                    ""name"": """",
-                    ""id"": ""70e0f303-c04f-4fd8-9e70-a5b9df0c46f0"",
-                    ""path"": ""<Gamepad>/leftTrigger"",
-                    ""interactions"": """",
-                    ""processors"": """",
-                    ""groups"": ""Gamepad"",
                     ""action"": ""Rewind"",
                     ""isComposite"": false,
                     ""isPartOfComposite"": false
@@ -242,23 +178,15 @@ public class @InputMaster : IInputActionCollection, IDisposable
                     ""isOR"": false
                 }
             ]
-        },
-        {
-            ""name"": ""Gamepad"",
-            ""bindingGroup"": ""Gamepad"",
-            ""devices"": [
-                {
-                    ""devicePath"": ""<Gamepad>"",
-                    ""isOptional"": false,
-                    ""isOR"": false
-                }
-            ]
         }
     ]
 }");
         // Player
         m_Player = asset.FindActionMap("Player", throwIfNotFound: true);
         m_Player_Movement = m_Player.FindAction("Movement", throwIfNotFound: true);
+        m_Player_Sprint = m_Player.FindAction("Sprint", throwIfNotFound: true);
+        m_Player_Crouch = m_Player.FindAction("Crouch", throwIfNotFound: true);
+        m_Player_Punch = m_Player.FindAction("Punch", throwIfNotFound: true);
         m_Player_Rewind = m_Player.FindAction("Rewind", throwIfNotFound: true);
     }
 
@@ -310,12 +238,18 @@ public class @InputMaster : IInputActionCollection, IDisposable
     private readonly InputActionMap m_Player;
     private IPlayerActions m_PlayerActionsCallbackInterface;
     private readonly InputAction m_Player_Movement;
+    private readonly InputAction m_Player_Sprint;
+    private readonly InputAction m_Player_Crouch;
+    private readonly InputAction m_Player_Punch;
     private readonly InputAction m_Player_Rewind;
     public struct PlayerActions
     {
-        private @InputMaster m_Wrapper;
-        public PlayerActions(@InputMaster wrapper) { m_Wrapper = wrapper; }
+        private @InputMaster1 m_Wrapper;
+        public PlayerActions(@InputMaster1 wrapper) { m_Wrapper = wrapper; }
         public InputAction @Movement => m_Wrapper.m_Player_Movement;
+        public InputAction @Sprint => m_Wrapper.m_Player_Sprint;
+        public InputAction @Crouch => m_Wrapper.m_Player_Crouch;
+        public InputAction @Punch => m_Wrapper.m_Player_Punch;
         public InputAction @Rewind => m_Wrapper.m_Player_Rewind;
         public InputActionMap Get() { return m_Wrapper.m_Player; }
         public void Enable() { Get().Enable(); }
@@ -329,6 +263,15 @@ public class @InputMaster : IInputActionCollection, IDisposable
                 @Movement.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMovement;
                 @Movement.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMovement;
                 @Movement.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMovement;
+                @Sprint.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnSprint;
+                @Sprint.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnSprint;
+                @Sprint.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnSprint;
+                @Crouch.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnCrouch;
+                @Crouch.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnCrouch;
+                @Crouch.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnCrouch;
+                @Punch.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnPunch;
+                @Punch.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnPunch;
+                @Punch.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnPunch;
                 @Rewind.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnRewind;
                 @Rewind.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnRewind;
                 @Rewind.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnRewind;
@@ -339,6 +282,15 @@ public class @InputMaster : IInputActionCollection, IDisposable
                 @Movement.started += instance.OnMovement;
                 @Movement.performed += instance.OnMovement;
                 @Movement.canceled += instance.OnMovement;
+                @Sprint.started += instance.OnSprint;
+                @Sprint.performed += instance.OnSprint;
+                @Sprint.canceled += instance.OnSprint;
+                @Crouch.started += instance.OnCrouch;
+                @Crouch.performed += instance.OnCrouch;
+                @Crouch.canceled += instance.OnCrouch;
+                @Punch.started += instance.OnPunch;
+                @Punch.performed += instance.OnPunch;
+                @Punch.canceled += instance.OnPunch;
                 @Rewind.started += instance.OnRewind;
                 @Rewind.performed += instance.OnRewind;
                 @Rewind.canceled += instance.OnRewind;
@@ -355,18 +307,12 @@ public class @InputMaster : IInputActionCollection, IDisposable
             return asset.controlSchemes[m_KeyboardandMouseSchemeIndex];
         }
     }
-    private int m_GamepadSchemeIndex = -1;
-    public InputControlScheme GamepadScheme
-    {
-        get
-        {
-            if (m_GamepadSchemeIndex == -1) m_GamepadSchemeIndex = asset.FindControlSchemeIndex("Gamepad");
-            return asset.controlSchemes[m_GamepadSchemeIndex];
-        }
-    }
     public interface IPlayerActions
     {
         void OnMovement(InputAction.CallbackContext context);
+        void OnSprint(InputAction.CallbackContext context);
+        void OnCrouch(InputAction.CallbackContext context);
+        void OnPunch(InputAction.CallbackContext context);
         void OnRewind(InputAction.CallbackContext context);
     }
 }
---
+++ Brackeys Game Jam/Assets/Scripts/Player/PlayerMovementController.cs
@@ -0,0 +1,156 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.InputSystem;
+
+public class PlayerMovementController : MonoBehaviour
+{
+    public InputMaster1 controls;
+    public Rigidbody2D rb;
+    public Transform punchPoint;
+    public GameObject PunchRadiusPF;
+
+    public Vector2 direction;
+
+    public float speed = 5f;
+    public float sprintspeed = 8f;
+    public float walkspeed = 5f;
+    public float crouch = 2f;
+    
+    public bool isCrouching;
+    //private float timebetweenPunch;
+    //public float startTimebetweenPunch;
+
+
+    [Header("Rewind")]
+
+    public float maxRewindTime;
+    public float RewindLeft;
+    public float RewindCooldown;
+    public float SaveOffset;
+    public Slider Rewind;
+    public GameObject RewindGUI;
+
+    private Rewind[] RewindScripts;
+    public bool RewindStarted;
+
+
+    private void Awake()
+    {
+        controls = new InputMaster1();
+
+        // Punch
+        controls.Player.Punch.performed += _ => Punch();
+
+        // Sprint
+        controls.Player.Sprint.performed += _ => Sprint();
+        controls.Player.Sprint.canceled += _ => Walk();
+
+        // Crouch
+        controls.Player.Crouch.performed += _ => Crouch();
+        controls.Player.Crouch.canceled += _ => Walk();
+
+        // Rewind
+        RewindScripts = FindObjectsOfType<Rewind>();
+        Rewind.maxValue = maxRewindTime;
+        controls.Player.Rewind.performed += _ => StartRewind();
+        controls.Player.Rewind.canceled += _ => StopRewind();
+
+    }
+    void Update()
+    {
+        Vector2 direction = controls.Player.Movement.ReadValue<Vector2>();
+        rb.velocity = direction * speed;
+
+        if (direction == Vector2.zero)
+            isCrouching = true;
+
+            Rewind.value = RewindLeft;
+        if (!RewindStarted)
+        {
+            if (RewindLeft < maxRewindTime)
+                RewindLeft += Time.deltaTime;
+            else
+                RewindLeft = maxRewindTime;
+            RewindCooldown -= Time.deltaTime;
+        }
+        else
+        {
+            RewindLeft -= Time.deltaTime;
+            if(RewindLeft < 0)
+            {
+                RewindLeft = 0;
+                StopRewind();
+            }
+
+        }
+
+
+
+    }
+
+    void Sprint()
+    {
+        speed = sprintspeed;
+    }
+
+    void Walk()
+    {
+        isCrouching = false;
+        speed = walkspeed; 
+    }
+    
+    void Crouch()
+    {
+        isCrouching = true;
+        speed = crouch;
+    }
+
+    void Punch()
+    {
+        Instantiate(PunchRadiusPF, punchPoint.position, punchPoint.rotation);
+        Debug.Log("Punched");
+    }
+
+    void StartRewind()
+    {
+        if (RewindLeft > 0 && RewindCooldown < 0)
+        {
+            RewindStarted = true;
+            RewindGUI.SetActive(true);
+
+            foreach (Rewind rewind in RewindScripts)
+            {
+                rewind.RewindTime();
+            }
+
+        }
+        else
+        {
+            StopRewind();
+        }
+    }
+
+    void StopRewind()
+    {
+        RewindStarted = false;
+
+        RewindGUI.SetActive(false);
+        RewindCooldown = maxRewindTime - RewindLeft;
+
+        foreach (Rewind rewind in RewindScripts)
+        {
+            rewind.StopRewindTime();
+        }
+    }
+
+    private void OnEnable()
+    {
+        controls.Enable();
+    }
+    private void OnDisable()
+    {
+        controls.Disable();
+    }
+}
